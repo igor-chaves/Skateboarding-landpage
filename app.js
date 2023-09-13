f0bef49c1e37fce7726c4ed9b2f06493db16df3d
Igor Chaves Donegá <ichaves@live.com>
2023-09-13 13:25:43 +0900

    fixed menu function[2]
    -Tried to pull commit and code got all messed up. Fixed again

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const menuIcon = document.getElementById("menu")
 const menuCloseIcon = document.getElementById("close-menu")
 const navLinks = document.getElementById("menu-active")
 
-
+// hide menu icon
 menu.addEventListener("click", () => {
    menuIcon.style.display = "none"
    menuCloseIcon.style.display = "block"
    navLinks.classList.add("active")
 })
 
+// hide close menu icon
 menuCloseIcon.addEventListener("click", () => {
    menuIcon.style.display = "block"
    menuCloseIcon.style.display = "none"
    navLinks.classList.remove("active")
+})
+
+// verify screen size
+window.addEventListener("resize", () => {
+   if (window.innerWidth > 690) {
+      navLinks.classList.remove("active")
+      menuIcon.style.display = "none"
+      menuCloseIcon.style.display = "none"
+   }
+})
+
+// show menu icon when screen is small
+window.addEventListener("resize", () => {
+   if (window.innerWidth < 690 && menuCloseIcon.style.display === "none") {
+      menuIcon.style.display = "block";
+   }
 })
